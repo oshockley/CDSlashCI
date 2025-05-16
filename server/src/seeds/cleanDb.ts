99ad5d6c89b1304fb8d1585430b00b6092be6b0c
O'Neal Shockley <shockleyoneal@gmail.com>
2025-05-15 18:11:48 -0700

    deleted more lines from cleanDB

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,15 +3,8 @@ import db from '../config/connection.js';
 
 export default async (modelName: 'Question', collectionName: string) => {
   try {
-    const model = models[modelName];
-
-    if (!model || !model.db || !model.db.db) {
-      throw new Error(`Model or database reference is undefined for "${modelName}"`);
-    }
-
-    const modelExists = await model.db.db
-      .listCollections({ name: collectionName })
-      .toArray();
+    
+    const modelExists = await models[modelName].db.db.listCollections({ name: collectionName }).toArray();
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
